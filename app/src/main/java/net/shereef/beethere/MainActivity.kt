4285abb4766fc0ca328ce1dced9934a28f10d9e7
Shereef Marzouk <git@shereef.net>
2017-11-11 11:16:55 -0500

    Initial commit

---
+++ app/src/main/java/net/shereef/beethere/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.shereef.beethere
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

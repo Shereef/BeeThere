202a50cbd249d6e790fd9fd9b302400d9d8968af
Shereef Marzouk <git@shereef.net>
2017-11-12 15:34:50 -0500

    Added Fabric.io, Crashlytics and Answers

--- app/src/main/java/net/shereef/beethere/MainActivity.kt
+++ app/src/main/java/net/shereef/beethere/MainActivity.kt
@@ -1,11 +1,20 @@
 package net.shereef.beethere
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+import com.crashlytics.android.answers.Answers
+
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+        Fabric.with(this, Answers())
+
+        //todo: Check here if there is a user logged in and direct them to the landing page
+
     }
 }
